8d279bf53b460258dcd0e1568ebff06552b35dfb
LucasAlonso11800 <lucasalonso11800@gmail.com>
2022-07-27 15:22:47 -0300

    Added cities routes and form validation

---
+++ pages/api/user/cities.ts
@@ -0,0 +1,66 @@
+// Middleware
+import connectDB from "../../../middlewares/mongo";
+// Model
+import User from "../../../models/User";
+// Utils
+import { checkAuth } from "../../../utils";
+// Types
+import { NextApiRequest, NextApiResponse } from "next";
+import { UserType } from "../../../types";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    
+    checkAuth(req.body.token);
+    
+    // Get user favorite cities
+    if (req.method === "GET") {
+        try {
+            const user: UserType | null = await User.findOne({ email: req.body.email });
+            if (!user) return res.json({ code: 0, cities: [] });
+            return res.json({
+                code: 1,
+                cities: user.cities
+            });
+        }
+        catch (err: any) {
+            throw new Error(err)
+        }
+    };
+
+    // Add city to favorites
+    if (req.method === "POST") {
+        try {
+            const user: UserType | null = await User.findOne({ email: req.body.email });
+            if (!user) return res.json({ code: 0, message: "User not found" });
+            if (user.cities.find(city => city === req.body.city)) return res.json({ code: 0, message: "City already saved" });
+            await User.updateOne({ _id: user._id }, { cities: [...user.cities, req.body.city] })
+            return res.json({
+                code: 1,
+                cities: [...user.cities, req.body.city]
+            })
+        }
+        catch (err: any) {
+            throw new Error(err)
+        }
+    };
+
+    // Delete city from favorites
+    if (req.method === "DELETE") {
+        try {
+            const user: UserType | null = await User.findOne({ email: req.body.email });
+            if (!user) return res.json({ code: 0, message: "User not found" });
+            if (!user.cities.find(city => city === req.body.city)) return res.json({ code: 0, message: "City not saved" });
+            await User.updateOne({ _id: user._id }, { cities: user.cities.filter(city => city !== req.body.city) });
+
+            return res.json({
+                code: 1,
+                cities: user.cities.filter(city => city !== req.body.city)
+            })
+        }
+        catch (err: any) {
+            throw new Error(err)
+        }
+    };
+};
+
+export default connectDB(handler);
--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -8,23 +8,38 @@ import bcrypt from 'bcryptjs';
 // Types
 import { NextApiRequest, NextApiResponse } from "next";
 import { UserType } from "../../../types";
+// Const
+import { emailRegex } from "../../../const/const";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
+        // Form validation
+        if (!req.body.email) return res.json({ code: 0, message: 'Please add an email' });
+        if (!emailRegex.test(req.body.email)) return res.json({ code: 0, message: 'Please enter a valid email' });
+
+        if (!req.body.password) return res.json({ code: 0, message: 'Please add a password' });
+        if (req.body.password.length < 6) return res.json({ code: 0, message: 'Password too short' });
+        if (req.body.password.length > 20) return res.json({ code: 0, message: 'Password too long' });
+
+        // Check existing email
         const email: UserType | null = await User.findOne({ email: req.body.email });
-        if (email) return res.json({ code: 0, message: 'Email already registered' })
+        if (email) return res.json({ code: 0, message: 'Email already registered' });
 
         const newUser: UserType = new User({
             email: req.body.email,
             password: req.body.password,
             cities: []
         });
+
+        // Hash password
         const salt: string = await bcrypt.genSalt(10);
         newUser.password = await bcrypt.hash(req.body.password, salt);
 
+        // Insert user in db
         const users: UserType[] = await User.insertMany(newUser) as any;
+        
+        // Generate jwt
         const token: string = generateToken(users[0]);
-
         res.json({
             _id: users[0]._id,
             token,
